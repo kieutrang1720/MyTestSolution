573549fd89fe48805b91f5c48f4e5d8bcd07d5f3
Kieu Trang <kieutrang1720@gmail.com>
2020-03-18 05:06:27 +0100

    Add project files.

---
+++ MyTestSolution/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
